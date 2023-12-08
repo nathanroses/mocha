6e7e34d549a7972adbf875f89a3ffe0eebf0860b
Nathan Rose <natetron99@gmail.com>
2023-12-07 22:32:33 -0500

    updated site

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -5,8 +5,9 @@ export const getPineconeClient = async () => {
 
   await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-east1-gcp',
+    environment: 'us-east4-gcp',
   })
 
   return client
 }
+
