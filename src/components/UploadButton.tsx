0bcd46ced720833e9389ea3700e0340270ca7b42
Nathan Rose <natetron99@gmail.com>
2023-12-08 01:27:48 -0500

    major fix

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -130,14 +130,10 @@ const UploadDropzone = ({
               {isUploading ? (
                 <div className='w-full mt-4 max-w-xs mx-auto'>
                   <Progress
-                    indicatorColor={
-                      uploadProgress === 100
-                        ? 'bg-green-500'
-                        : ''
-                    }
-                    value={uploadProgress}
-                    className='h-1 w-full bg-zinc-200'
+                  value={uploadProgress}
+                 className={`${uploadProgress === 100 ? 'bg-green-500' : ''} ...otherClasses`}
                   />
+
                   {uploadProgress === 100 ? (
                     <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
                       <Loader2 className='h-3 w-3 animate-spin' />
