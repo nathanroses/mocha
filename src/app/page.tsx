30bff9943c66409a97db0951951a69c2692a0761
Nathan Rose <natetron99@gmail.com>
2024-01-10 08:44:44 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
           Evaluate your {' '}
-          <span className='text-blue-700'>code</span>{' '}
+          <span className='text-blue-800'>code</span>{' '}
           in seconds.
         </h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
