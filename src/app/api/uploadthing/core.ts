da3e424e463b0803460bf96c96eb123246002ad8
Nathan Rose <contacttstyle@gmail.com>
2025-03-21 01:11:42 -0500

    Update core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -13,7 +13,7 @@ import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { PLANS } from '@/config/stripe'
 
 // Import the PDFLoader differently
-import { PDFLoader } from 'langchain/document_loaders/web/pdf'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 
 const f = createUploadthing()
 
