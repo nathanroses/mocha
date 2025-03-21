d69b1bda44968da1666e88587f680ce4c2a78454
Nathan Rose <contacttstyle@gmail.com>
2025-03-21 00:17:56 -0500

    Update MaxWidthWrapper.tsx

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,15 +1,27 @@
 import { cn } from '@/lib/utils'
 import { ReactNode } from 'react'
 
+interface MaxWidthWrapperProps {
+  className?: string
+  children: ReactNode
+  noPadding?: boolean
+}
+
 const MaxWidthWrapper = ({
   className,
   children,
-}: {
-  className?: string
-  children: ReactNode
-}) => {
+  noPadding = false,
+}: MaxWidthWrapperProps) => {
   return (
-    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+    <div 
+      className={cn(
+        'mx-auto w-full max-w-screen-xl',
+        {
+          'px-2.5 md:px-20': !noPadding,
+        },
+        className
+      )}
+    >
       {children}
     </div>
   )
